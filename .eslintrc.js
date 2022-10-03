37914824e6e147a59082934cfb75d090fcc83a6e
Hgc <2484203027@qq.com>
2022-10-03 12:37:06 +0800

    目录调整

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,16 @@ module.exports = {
   env: {
     node: true
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/standard'
-  ],
+  extends: ['plugin:vue/vue3-essential', '@vue/standard'],
   parserOptions: {
     parser: '@babel/eslint-parser'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // 在方法的形参 () 之前，是否必须有空格
+    'space-before-function-paren': 0,
+    // 关闭组件命名规则
+    'vue/multi-word-component-names': 'off'
   }
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,16 @@
 import { createStore } from 'vuex'
 
+// vue2.0 创建仓库 new Vuex.Store({})
+// vue3.0 创建仓库 createStore({})
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  // 数据
+  state: {},
+  // vuex的计算属性
+  getters: {},
+  // 改数据函数
+  mutations: {},
+  // 请求数据函数
+  actions: {},
+  // 分模块
+  modules: {}
 })
