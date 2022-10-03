0daa63fd67a39e57140497461aa40a9b8b7e4aed
Hgc <2484203027@qq.com>
2022-10-03 13:37:20 +0800

    实验2

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,34 @@
 <template>
   <div>
     <h1>App 根组件</h1>
-    <!-- 1. 使用根模块state的数据   -->
-    <p>{{ $store.state.username }}</p>
-    <!-- 2. 使用根模块getters的数据 -->
+    <!-- 1.1 使用模块A的username -->
+    <p>{{ $store.state.moduleA.username }}</p>
+    <!-- 1.2 使用A模块getters的数据 -->
     <p>{{ $store.getters.newName }}</p>
-    <button @click="mutationsFn">改名字</button>
+    <!-- 2.1 使用模块B的username -->
+    <p>{{ $store.state.moduleB.username }}</p>
+    <!-- 2.2 使用B模块getters的数据 -->
+    <p>{{ $store.getters['moduleB/newName'] }}</p>
+    <button @click="mutationsFn">mutationsFn</button>
+    <button @click="actionsFn">actionsFn</button>
   </div>
 </template>
 
 <script>
-// 可以通过调用 useStore 函数，来在 setup 钩子函数中访问 store
 import { useStore } from 'vuex'
+// 可以通过调用 useStore 函数，来在 setup 钩子函数中访问 store
 export default {
   name: 'App',
   setup() {
-    // 1. 使用根模块state的数据
     const store = useStore()
-    console.log(store.state.username)
-    // 2. 使用根模块getters的数据
-    console.log(store.getters.newName)
-    // 3. 提交根模块mutations函数
-    // 要使用 mutation 和 action 时，只需要在 setup 钩子函数中调用 commit 和 dispatch 函数。
     const mutationsFn = () => {
-      // store.commit('updateName')
-      // 4. 调用根模块actions函数
-      store.dispatch('updateName')
+      // 2.3 提交B模块的修改
+      store.commit('moduleB/updateName')
     }
-    return { mutationsFn }
+    const actionsFn = () => {
+      store.dispatch('moduleB/updateName')
+    }
+    return { mutationsFn, actionsFn }
   }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -2,15 +2,28 @@ import { createStore } from 'vuex'
 
 // vue2.0 创建仓库 new Vuex.Store({})
 // vue3.0 创建仓库 createStore({})
-export default createStore({
-  // 数据
+// A 模块
+const moduleA = {
+  // 默认的模块，`state` 区分模块，其他 `getters` `mutations` `actions` 都在全局。
   state: {
-    username: 'zs'
+    username: 'moduleA'
   },
-  // vuex的计算属性
   getters: {
     newName(state) {
-      return state.username + ' getters 计算属性'
+      return state.username + 'A !!!'
+    }
+  }
+}
+// B 模块
+const moduleB = {
+  // 带命名空间 `namespaced: true` 的模块，所有功能区分模块，更高封装度和复用。
+  namespaced: true,
+  state: {
+    username: 'moduleB'
+  },
+  getters: {
+    newName(state) {
+      return state.username + 'BBBBBBB'
     }
   },
   // 改数据函数
@@ -26,7 +39,21 @@ export default createStore({
         ctx.commit('updateName')
       }, 1000)
     }
-  },
+  }
+}
+
+export default createStore({
+  // 数据
+  state: {},
+  // vuex的计算属性
+  getters: {},
+  // 改数据函数
+  mutations: {},
+  // 请求数据函数
+  actions: {},
   // 分模块
-  modules: {}
+  modules: {
+    moduleA,
+    moduleB
+  }
 })
